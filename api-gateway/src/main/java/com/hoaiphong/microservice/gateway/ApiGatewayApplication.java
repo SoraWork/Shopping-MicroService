5c50fb61f110ece67c91948eb6dfe9ea92ae1111
HoaiPhong <phongnguyenhoai2003@gmail.com>
2025-05-09 14:22:21 +0700

    API Gateway

---
+++ api-gateway/src/main/java/com/hoaiphong/microservice/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.hoaiphong.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
